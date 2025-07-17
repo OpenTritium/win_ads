a5a0aeea562dc6029c8de3427935da202e92a317
OpenTritium <tritium404@hotmail.com>
2025-07-17 22:22:57 +0800

    Refactor Cargo.toml and lib.rs to streamline dependencies and improve Windows-specific code organization

--- src/lib.rs
+++ src/lib.rs
@@ -1,98 +1,104 @@
 #[cfg(not(windows))]
-compile_error!("This crate is designed to work only on the Windows operating system.");
+compile_error!("This crate is only supported on Windows.");
 
-use windows::{
-    Win32::{
-        Foundation::{CloseHandle, GENERIC_READ, GENERIC_WRITE},
-        Storage::FileSystem::{
-            CREATE_ALWAYS, CreateFileW, DeleteFileW, FILE_APPEND_DATA, FILE_ATTRIBUTE_NORMAL,
-            FILE_SHARE_NONE, FILE_SHARE_READ, GetFileSizeEx, OPEN_ALWAYS, OPEN_EXISTING, ReadFile,
-            WriteFile,
+#[cfg(windows)]
+pub use windows_only::*;
+
+#[cfg(any(windows, doc))]
+mod windows_only {
+    use windows::{
+        Win32::{
+            Foundation::{CloseHandle, GENERIC_READ, GENERIC_WRITE},
+            Storage::FileSystem::{
+                CREATE_ALWAYS, CreateFileW, DeleteFileW, FILE_APPEND_DATA, FILE_ATTRIBUTE_NORMAL,
+                FILE_SHARE_NONE, FILE_SHARE_READ, GetFileSizeEx, OPEN_ALWAYS, OPEN_EXISTING,
+                ReadFile, WriteFile,
+            },
         },
-    },
-    core::{PCWSTR, Result},
-};
-use windows_strings::HSTRING;
+        core::{PCWSTR, Result},
+    };
+    use windows_strings::HSTRING;
 
-/// create or overwrite an ads
-#[inline]
-pub fn set_ads(path: &str, name: &str, src: impl AsRef<[u8]>) -> Result<u32> {
-    let stream_path = HSTRING::from(format!("{path}:{name}"));
-    let mut written: u32 = 0;
-    unsafe {
-        let handle = CreateFileW(
-            PCWSTR::from_raw(stream_path.as_ptr()),
-            GENERIC_WRITE.0,
-            FILE_SHARE_NONE,
-            None,
-            CREATE_ALWAYS,
-            FILE_ATTRIBUTE_NORMAL,
-            None,
-        )?;
-        WriteFile(handle, Some(src.as_ref()), Some(&mut written), None)?;
-        CloseHandle(handle)?;
+    /// create or overwrite an ads
+    #[inline]
+    pub fn set_ads(path: &str, name: &str, src: impl AsRef<[u8]>) -> Result<u32> {
+        let stream_path = HSTRING::from(format!("{path}:{name}"));
+        let mut written: u32 = 0;
+        unsafe {
+            let handle = CreateFileW(
+                PCWSTR::from_raw(stream_path.as_ptr()),
+                GENERIC_WRITE.0,
+                FILE_SHARE_NONE,
+                None,
+                CREATE_ALWAYS,
+                FILE_ATTRIBUTE_NORMAL,
+                None,
+            )?;
+            WriteFile(handle, Some(src.as_ref()), Some(&mut written), None)?;
+            CloseHandle(handle)?;
+        }
+        Ok(written)
     }
-    Ok(written)
-}
 
-/// read an ads
-#[inline]
-pub fn get_ads(path: &str, name: &str, dst: &mut Vec<u8>) -> Result<()> {
-    let stream_path = HSTRING::from(format!("{path}:{name}"));
-    unsafe {
-        let handle = CreateFileW(
-            PCWSTR::from_raw(stream_path.as_ptr()),
-            GENERIC_READ.0,
-            FILE_SHARE_READ,
-            None,
-            OPEN_EXISTING,
-            FILE_ATTRIBUTE_NORMAL,
-            None,
-        )?;
-        let mut file_size = 0i64;
-        GetFileSizeEx(handle, &mut file_size)?;
-        dst.resize(file_size as usize, 0);
-        let mut read = 0u32;
-        ReadFile(handle, Some(dst.as_mut()), Some(&mut read), None)?;
-        CloseHandle(handle)?;
-        dst.set_len(read as usize);
+    /// read an ads
+    #[inline]
+    pub fn get_ads(path: &str, name: &str, dst: &mut Vec<u8>) -> Result<()> {
+        let stream_path = HSTRING::from(format!("{path}:{name}"));
+        unsafe {
+            let handle = CreateFileW(
+                PCWSTR::from_raw(stream_path.as_ptr()),
+                GENERIC_READ.0,
+                FILE_SHARE_READ,
+                None,
+                OPEN_EXISTING,
+                FILE_ATTRIBUTE_NORMAL,
+                None,
+            )?;
+            let mut file_size = 0i64;
+            GetFileSizeEx(handle, &mut file_size)?;
+            dst.resize(file_size as usize, 0);
+            let mut read = 0u32;
+            ReadFile(handle, Some(dst.as_mut()), Some(&mut read), None)?;
+            CloseHandle(handle)?;
+            dst.set_len(read as usize);
+            Ok(())
+        }
+    }
+
+    /// append an ads
+    #[inline]
+    pub fn append_ads(path: &str, name: &str, src: impl AsRef<[u8]>) -> Result<u32> {
+        let stream_path = HSTRING::from(format!("{path}:{name}"));
+        let mut written = 0;
+        unsafe {
+            let handle = CreateFileW(
+                PCWSTR::from_raw(stream_path.as_ptr()),
+                FILE_APPEND_DATA.0,
+                FILE_SHARE_NONE,
+                None,
+                OPEN_ALWAYS,
+                FILE_ATTRIBUTE_NORMAL,
+                None,
+            )?;
+            WriteFile(handle, Some(src.as_ref()), Some(&mut written), None)?;
+            CloseHandle(handle)?;
+        }
+        Ok(written)
+    }
+
+    /// delete a ads
+    #[inline]
+    pub fn delete_ads(path: &str, name: &str) -> Result<()> {
+        let stream_path = HSTRING::from(format!("{path}:{name}"));
+        unsafe {
+            DeleteFileW(PCWSTR::from_raw(stream_path.as_ptr()))?;
+        }
         Ok(())
     }
 }
 
-/// append an ads
-#[inline]
-pub fn append_ads(path: &str, name: &str, src: impl AsRef<[u8]>) -> Result<u32> {
-    let stream_path = HSTRING::from(format!("{path}:{name}"));
-    let mut written = 0;
-    unsafe {
-        let handle = CreateFileW(
-            PCWSTR::from_raw(stream_path.as_ptr()),
-            FILE_APPEND_DATA.0,
-            FILE_SHARE_NONE,
-            None,
-            OPEN_ALWAYS,
-            FILE_ATTRIBUTE_NORMAL,
-            None,
-        )?;
-        WriteFile(handle, Some(src.as_ref()), Some(&mut written), None)?;
-        CloseHandle(handle)?;
-    }
-    Ok(written)
-}
-
-/// delete a ads
-#[inline]
-pub fn delete_ads(path: &str, name: &str) -> Result<()> {
-    let stream_path = HSTRING::from(format!("{path}:{name}"));
-    unsafe {
-        DeleteFileW(PCWSTR::from_raw(stream_path.as_ptr()))?;
-    }
-    Ok(())
-}
-
 #[cfg(test)]
-#[cfg(target_os = "windows")]
+#[cfg(windows)]
 mod tests {
     use super::*;
     use tempfile::NamedTempFile;
